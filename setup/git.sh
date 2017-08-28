eb8b42332626bb84329220fb1f244b59627c4233
Kevin Decker <kpdecker@gmail.com>
2017-08-28 15:37:04 -0500

    Add git alias setup

---
+++ setup/git.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export PATH=/usr/local/bin/:$PATH
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.cp cherry-pick
+
+git config --global alias.p push
+git config --global alias.pf 'push -f'
