cbcab10bdfaffebdef72a651010f91f34567d4c2
Kevin Decker <kpdecker@gmail.com>
2017-08-28 15:30:03 -0500

    Reset minimize to dock flag

--- osx-defaults/dock.sh
+++ osx-defaults/dock.sh
@@ -13,7 +13,7 @@ defaults write com.apple.dock tilesize -int 23
 defaults write com.apple.dock mineffect -string "scale"
 
 # Minimize windows into their application’s icon
-defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock minimize-to-application -bool false
 
 # Enable spring loading for all Dock items
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
