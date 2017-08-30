578f31f8fb60d7f2b0db083b9c0d9a495c905c56
Kevin Decker <kpdecker@gmail.com>
2017-08-30 08:23:57 -0500

    Move osx defaults into setup folder

---
+++ setup/osx-defaults.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+for path in ~/.dotfiles/setup/osx-defaults/*; do
+  $path
+done
+
+for app in "Activity Monitor" \
+  "Address Book" \
+  "Calendar" \
+  "cfprefsd" \
+  "Contacts" \
+  "Dock" \
+  "Google Chrome Canary" \
+  "Google Chrome" \
+  "Mail" \
+  "Messages" \
+  "Photos" \
+  "Safari" \
+  "SystemUIServer" \
+  "Terminal" \
+  "Transmission" \
+  "Twitter" \
+  "iCal"; do
+  killall "${app}" &> /dev/null
+done
