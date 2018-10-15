17144961098eba8c92fa38469cb31cc26bbb9cb7
Kevin Decker <kpdecker@gmail.com>
2018-10-15 11:42:39 -0500

    Update git config init

--- setup/git.sh
+++ setup/git.sh
@@ -2,6 +2,8 @@
 
 export PATH=/usr/local/bin/:$PATH
 
+git config --global push.followTags true
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
@@ -10,6 +12,8 @@ git config --global alias.cp cherry-pick
 
 git config --global alias.p push
 git config --global alias.pf 'push -f'
+git config --global alias.pfn 'push -f --no-verify'
+git config --global alias.pn 'push --no-verify'
 
 git config --global alias.ra 'rebase --abort'
 git config --global alias.rc 'rebase --continue'
