bef55bab0ea1e9ad446349dc5c63eb64d48bd367
Kevin Decker <kpdecker@gmail.com>
2020-06-04 13:36:51 -0500

    Only exec nvm when it exists

--- profile.d/node.sh
+++ profile.d/node.sh
@@ -1,8 +1,11 @@
 alias cov='open `find . -path ./node_modules -prune -o -path "*/lcov-report/index.html" -print`'
 
-. ~/.nvm/nvm.sh
+if [ -f ~/.nvm/nvm.sh ]; then
+  . ~/.nvm/nvm.sh
+
+  export PATH="$HOME/.yarn/bin:$PATH"
+fi
 
 export PATH=~/.config/yarn/global/node_modules/.bin/:$PATH
 export PATH=./node_modules/.bin:$PATH
 
-export PATH="$HOME/.yarn/bin:$PATH"
