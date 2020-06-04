9ccabb4690f439e931c41dc808a3bec0177f07da
Kevin Decker <kpdecker@gmail.com>
2020-06-04 13:29:29 -0500

    Add ls listing flags to color envs

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -6,12 +6,13 @@ alias .....="cd ../../../.."
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
   test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-  alias ls='ls --color=auto'
+  alias ls='ls -lhaF --color=auto'
 
   alias grep='grep --color=auto'
   alias fgrep='fgrep --color=auto'
   alias egrep='egrep --color=auto'
 else
+  # OSX has no --color, but default
   alias ls="ls -lhaF"
 fi
 
