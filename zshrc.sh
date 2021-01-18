3f1e93a55d515921a3fdc1f6618c6d5c0d0d62e1
Kevin Decker <kpdecker@gmail.com>
2021-01-18 10:38:46 -0600

    Add zsh timer

--- zshrc.sh
+++ zshrc.sh
@@ -1,8 +1,25 @@
+function timer_start {
+  # timerCmd=$(history 1 | sed -e 's/^[ 0-9]*//')
+  # echo $timerCmd
+  if [ -z "$timer" ]; then
+    timer=${timer:-$SECONDS}
+  fi
+}
+
+function timer_stop {
+  if [ -n "$timer" ]; then
+    timer_show=$(($SECONDS - $timer))
+    unset timer
+  fi
+}
+
 if [[ "$-" = *i* ]]; then
   export BASH_INTERACTIVE=true
 else
   export BASH_INTERACTIVE=false
 fi
+
+[[ -z $precmd_functions ]] && precmd_functions=()
 
 # Profile content
 source ~/.dotfiles/env.sh
@@ -14,3 +31,12 @@ done
 for scriptPath in ~/.dotfiles/zsh_profile.d/*; do
   source $scriptPath
 done
+
+
+if [ "$BASH_INTERACTIVE" = true ]; then
+  # Shared history for all shells
+  setopt INC_APPEND_HISTORY
+
+  trap 'timer_start' DEBUG
+  precmd_functions=($precmd_functions timer_stop)
+fi
